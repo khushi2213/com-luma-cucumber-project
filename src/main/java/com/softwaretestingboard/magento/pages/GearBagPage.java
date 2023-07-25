7e35df0c10a58dd4ced72e0387d51d7b208e5a9f
khushi2213 <kkhushee.khushi@gmail.com>
2023-07-25 22:17:36 +0100

    Done

---
+++ src/main/java/com/softwaretestingboard/magento/pages/GearBagPage.java
@@ -0,0 +1,17 @@
+package com.softwaretestingboard.magento.pages;
+
+import com.softwaretestingboard.magento.utility.Utility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+
+public class GearBagPage extends Utility {
+    @CacheLookup
+    @FindBy(xpath = "//a[normalize-space()='Overnight Duffle']")
+    WebElement overNightDuffle;
+
+    public void clickOnProductNameOvernightDuffle() throws InterruptedException {
+        Thread.sleep(500);
+        clickOnElement(overNightDuffle);
+    }
+}
